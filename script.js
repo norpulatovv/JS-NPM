f29b6955cdba12e05466e10ff1a6df2fee6ef424
norpulatovv <yoldoshevxasan98@gmail.com>
2025-09-17 15:22:46 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+var typed = new Typed('#element', {
+    strings: ['<h1>Text Gradients</h1>'],
+    typeSpeed: 50,
+});
+
+var typed = new Typed('#box1', {
+    strings: ['Make your headlines stand out'],
+    typeSpeed: 50,
+});
